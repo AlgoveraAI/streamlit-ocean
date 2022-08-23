06c408b663f2dbf23e71f27d64ea0647e5b68d36
Jakub Smékal <jakub.smekal@gmail.com>
2022-08-23 13:20:28 +0000

    Working FRE data and algo buying, code cleanup

--- streamlit_ocean/frontend/src/OceanAssetBuyComponent.tsx
+++ streamlit_ocean/frontend/src/OceanAssetBuyComponent.tsx
@@ -30,72 +30,10 @@ import {
   approveWei,
 } from '@oceanprotocol/lib'
 
-var axios = require('axios');
-
 declare global {
   interface AssetExtended extends Asset {
     accessDetails?: any
   }
-}
-
-
-async function query(datatokenAddress: string) {
-  const query = `{
-    token(id:"${datatokenAddress}", subgraphError: deny){
-      id
-      symbol
-      nft {
-        name
-        symbol
-        address
-      }
-      name
-      symbol
-      cap
-      isDatatoken
-      holderCount
-      orderCount
-      orders(skip:0,first:1){
-        amount
-        serviceIndex
-        payer {
-          id
-        }
-        consumer{
-          id
-        }
-        estimatedUSDValue
-        lastPriceToken
-        lastPriceValue
-      }
-    }
-    fixedRateExchanges(subgraphError:deny){
-      id
-      price
-      active
-    }
-  }`
-  
-  const baseUrl = "https://v4.subgraph.rinkeby.oceanprotocol.com"
-  const route = "/subgraphs/name/oceanprotocol/ocean-subgraph"
-  
-  const url = `${baseUrl}${route}`
-  
-  var config = {
-    method: 'post',
-    url: url,
-    headers: { "Content-Type": "application/json" },
-    data: JSON.stringify({ "query": query })
-  };
-  
-  var r = axios(config)
-    .then(function (response: { data: any; }) {
-      console.log(JSON.stringify(response.data.data));
-    })
-    .catch(function (error: any) {
-      console.log(error);
-    });
-  return r
 }
 
 interface State {
@@ -117,23 +55,24 @@ const getTestConfig = async (web3: Web3) => {
   return config
 }
 
-
 async function buyAsset(did: string, userAddress: string) {
     const accounts = await window.ethereum.request({
       method: 'eth_requestAccounts',
     });
     console.log("accounts", accounts)
+
     const config: any = await getTestConfig(web3)
     console.log("config", config)
+
     const aquarius = new Aquarius(config.metadataCacheUri)
     const providerUrl = config.providerUri
-    const datatoken = new Datatoken(web3)
-    const DATATOKEN_AMOUNT = '1'
     console.log("userAddress", userAddress[0])
+
     const consumerETHBalance = await web3.eth.getBalance(userAddress[0])
     console.log(`Consumer ETH balance: ${consumerETHBalance}`)
     let consumerOCEANBalance = await balance(web3, config.oceanTokenAddress, userAddress[0])
     console.log(`Consumer OCEAN balance before swap: ${consumerOCEANBalance}`)
+
     console.log('Did', did)
     const dt: AssetExtended = await aquarius.resolve(did)
     const resolvedDataDDO = await aquarius.waitForAqua(did);
@@ -142,12 +81,10 @@ async function buyAsset(did: string, userAddress: string) {
     const dtAddress = dt.datatokens[0].address
     console.log("dtAddress", dtAddress)
     console.log("dt.datatokens[0]" , dt.datatokens[0])
-    // let consumerDTBalance = await web3.eth.getBalance(userAddress[0], )
+
     let consumerDTBalance = await balance(web3, dtAddress, userAddress[0])
     console.log(`Consumer ${dt.datatokens[0].symbol} balance before swap: ${consumerDTBalance}`)
-    const r: any = await query(dtAddress)
-    console.log("r", r)
-    // console.log(r.data.fixedRateExchanges)
+  
     const freAddressRinkeby = config.fixedRateExchangeAddress
     console.log("config freAddressRinkeby", config.fixedRateExchangeAddress)
     const oceanAddressRinkeby = config.oceanTokenAddress
@@ -155,53 +92,7 @@ async function buyAsset(did: string, userAddress: string) {
     const fixedRate = new FixedRateExchange(web3, freAddressRinkeby)
     fixedRate.oceanAddress = oceanAddressRinkeby
     console.log("fixedRate", fixedRate)
-
-
-    // const freParams: any = {
-    //   fixedRateAddress: config.fixedRateExchangeAddress,
-    //   baseTokenAddress: config.oceanTokenAddress,
-    //   owner: dt.event.from,
-    //   marketFeeCollector: dt.event.from,
-    //   baseTokenDecimals: 18,
-    //   datatokenDecimals: 18,
-    //   fixedRate: '1',
-    //   marketFee: '0.001',
-    //   withMint: false
-    // }
-
     
-    // console.log("freParams", freParams)
-    
-    const computeEnvs = await ProviderInstance.getComputeEnvironments(providerUrl)
-    console.log("Available compute environments:", computeEnvs)
-    // let's have 2 minutes of compute access
-    const mytime = new Date()
-    const computeMinutes = 1
-    mytime.setMinutes(mytime.getMinutes() + computeMinutes)
-    let computeValidUntil = Math.floor(mytime.getTime() / 1000)
-    // const computeInit = await ProviderInstance.initializeCompute(resolvedDataDDO, algorithm, computeEnvs[0].id, computeValidUntil, providerUrl, userAddress[0])
-    
-    const initializeData =
-    (await ProviderInstance.initialize(
-      dt.id,
-      dt.services[0].id,
-      0,
-      userAddress[0],
-      dt.services[0].serviceEndpoint
-      ))
-      
-      const orderParams: any = {
-        consumer: computeEnvs[0].consumerAddress,
-        serviceIndex: 0,
-        _providerFee: initializeData.providerFee,
-        _consumeMarketFee: {
-          consumeMarketFeeAddress: '0x9984b2453eC7D99a73A5B3a46Da81f197B753C8d',
-          consumeMarketFeeAmount: '0',
-          consumeMarketFeeToken:
-          '0x0000000000000000000000000000000000000000'
-        }
-      }
-      
       const exchangeId = await fixedRate.generateExchangeId(config.oceanTokenAddress, dtAddress) // previously contracts.daiAddress
       console.log("exchangeId", exchangeId)
       const exchangeIds = await fixedRate.getExchanges()
@@ -226,142 +117,6 @@ async function buyAsset(did: string, userAddress: string) {
       const tx = await fixedRate.buyDT(userAddress[0], exchangeId, '1', '100')
       console.log("tx", tx)
 
-      // const freParams: any = {
-      //   exchangeContract: config.fixedRateExchangeAddress,
-      //   exchangeId: exchangeId,
-      //   maxBaseTokenAmount: '20',
-      //   baseTokenAddress: config.oceanTokenAddress,
-      //   baseTokenDecimals: config.oceanTokenAddress?.decimals || 18,
-      //   swapMarketFee: '0',
-      //   swapMarketFeeAddress: '0x9984b2453eC7D99a73A5B3a46Da81f197B753C8d'
-      // }
-
-      // console.log("freParams", freParams)
-
-
-    // const orderParams: any = {
-    //   consumer: userAddress[0],
-    //   serviceIndex: dt.datatokens[0].serviceId,
-    //   _providerFee: {
-    //     providerFeeAddress: user1,
-    //     providerFeeToken: providerFeeToken,
-    //     providerFeeAmount: providerFeeAmount,
-    //     v: v,
-    //     r: r,
-    //     s: s,
-    //     providerData: web3.utils.toHex(web3.utils.asciiToHex(providerData)),
-    //     validUntil: providerValidUntil
-    //   },
-    //   _consumeMarketFee: {
-    //     consumeMarketFeeAddress: "0x0000000000000000000000000000000000000000",
-    //     consumeMarketFeeToken: ZERO_ADDRESS,
-    //     consumeMarketFeeAmount: "0",
-    //   },
-    // };
-
-    // const accessDetails = await getAccessDetails(
-    //   resolvedDataDDO.chainId,
-    //   dtAddress,
-    //   3600, // TODO: valid until
-    //   userAddress[0], // previously payerAccount
-    // );
-    
-    // const orderPriceAndFees = await getOrderPriceAndFees(resolvedDataDDO, accessDetails, order.providerFee);
-
-    // const freParams: FreOrderParams = {
-    //   exchangeContract: config.fixedRateExchangeAddress,
-    //   exchangeId: accessDetails.addressOrId,
-    //   maxBaseTokenAmount: orderPriceAndFees.price,
-    //   baseTokenAddress: config.oceanTokenAddress,
-    //   baseTokenDecimals: 18, // TODO: Here we assume 18 decimal token, might not be the case
-    //   swapMarketFee: "0",
-    //   marketFeeAddress: "0x0000000000000000000000000000000000000000",
-    // };
-    // resolvedDataDDO.accessDetails.addressOrId
-
-    // const tx = await datatoken.buyFromFreAndOrder(dtAddress, userAddress[0], orderParams, freParams)
-    // console.log("tx", tx)
-    
-    // BELOW IS WRONG
-    // const nftParams: NftCreateData = {
-    //   name: dt.nft.name,
-    //   symbol: dt.nft.symbol,
-    //   templateIndex: 1,
-    //   tokenURI: '',
-    //   transferable: true,
-    //   owner: dt.event.from
-    // }
-
-    // const erc20Params: Erc20CreateParams = {
-    //   templateIndex: 1,
-    //   cap: '100000',
-    //   feeAmount: '0',
-    //   paymentCollector: ZERO_ADDRESS,
-    //   feeToken: ZERO_ADDRESS,
-    //   minter: dt.event.from,
-    //   mpFeeAddress: ZERO_ADDRESS
-    // }
-
-    
-    // const freNftAddress = tx.events.NFTCreated.returnValues[0]
-    // const freDatatokenAddress = tx.events.TokenCreated.returnValues[0]
-    // const freAddress = tx.events.NewFixedRate.returnValues.exchangeContract
-    // const freId = tx.events.NewFixedRate.returnValues.exchangeId
-
-
-    // consumerOCEANBalance = await balance(web3, addresses.Ocean, userAddress)
-    // console.log(`Consumer OCEAN balance after swap: ${consumerOCEANBalance}`)
-    // consumerDTBalance = await balance(web3, dtAddress, userAddress)
-    // console.log(`Consumer ${FRE_NFT_SYMBOL} balance after swap: ${consumerDTBalance}`)
-
-    // const resolvedDDO = await aquarius.waitForAqua(DDO.id)
-    // assert(resolvedDDO, 'Cannot fetch DDO from Aquarius')
-
-    // const initializeData = await ProviderInstance.initialize(
-    //   resolvedDDO.id,
-    //   resolvedDDO.services[0].id,
-    //   0,
-    //   userAddress,
-    //   providerUrl
-    // )
-
-    // const providerFees: ProviderFees = {
-    //   providerFeeAddress: initializeData.providerFee.providerFeeAddress,
-    //   providerFeeToken: initializeData.providerFee.providerFeeToken,
-    //   providerFeeAmount: initializeData.providerFee.providerFeeAmount,
-    //   v: initializeData.providerFee.v,
-    //   r: initializeData.providerFee.r,
-    //   s: initializeData.providerFee.s,
-    //   providerData: initializeData.providerFee.providerData,
-    //   validUntil: initializeData.providerFee.validUntil
-    // }
-
-    // const tx = await datatoken.startOrder(
-    //   dtAddress,
-    //   userAddress,
-    //   userAddress,
-    //   0,
-    //   providerFees
-    // )
-
-    // const downloadURL = await ProviderInstance.getDownloadUrl(
-    //   DDO.id,
-    //   userAddress,
-    //   DDO.services[0].id,
-    //   0,
-    //   tx.transactionHash,
-    //   providerUrl,
-    //   web3
-    // )
-
-    // console.log(`Download URL: ${downloadURL}`)
-
-    // consumerOCEANBalance = await balance(web3, addresses.Ocean, userAddress)
-    // console.log(`Consumer OCEAN balance after order: ${consumerOCEANBalance}`)
-    // consumerDTBalance = await balance(web3, dtAddress, userAddress)
-    // console.log(`Consumer ${FRE_NFT_SYMBOL} balance after order: ${consumerDTBalance}`)
-
-    // return did
 }
 /**
    * This is a React-based component template. The `render()` function is called
